574ac70e096889c67afb5646ccd350fa73f155be
Jacob Williams <jacobwilliams@users.noreply.github.com>
2022-06-19 00:10:23 -0500

    formatting

--- src/mach_routines.f90
+++ src/mach_routines.f90
@@ -1,8 +1,16 @@
 !*****************************************************************************************
 !>
 !  Fortran machine constants.
+!
+!### Reference
+!  * P. A. Fox, A. D. Hall and N. L. Schryer, Framework for
+!    a portable library, ACM Transactions on Mathematical
+!    Software 4, 2 (June 1978), pp. 177-188.
+!
+!### Author
+!  * Jacob Williams, June 2022
 
-module mach_routines
+   module mach_routines
 
    use iso_fortran_env
 
@@ -10,12 +18,12 @@ module mach_routines
 
    private
 
-   integer, parameter :: sp = kind(1.0)
-   integer, parameter :: dp = kind(1.0d0)
+   integer, parameter :: sp = kind(1.0)    !! single precision kind
+   integer, parameter :: dp = kind(1.0d0)  !! double precision kind
 
    public :: r1mach, d1mach, i1mach
 
-contains
+   contains
 !*****************************************************************************************
 
 !*******************************************************************
@@ -57,18 +65,14 @@ contains
 !  * `I1MACH(13) = EMAX`, the largest exponent E.
 !
 !### Author
-!  Fox, P. A., (Bell Labs)
-!  Hall, A. D., (Bell Labs)
-!  Schryer, N. L., (Bell Labs)
+!  * Fox, P. A., (Bell Labs)
+!  * Hall, A. D., (Bell Labs)
+!  * Schryer, N. L., (Bell Labs)
 !
-!***REFERENCES
-!  * P. A. Fox, A. D. Hall and N. L. Schryer, Framework for
-!    a portable library, ACM Transactions on Mathematical
-!    Software 4, 2 (June 1978), pp. 177-188.
-!
-!### REVISION HISTORY
+!### Revision history
 !  * 790101  DATE WRITTEN
 !  * 960329  Modified for Fortran 90 (BE after suggestions by EHG)
+!  * 220619  Modified for Fortran 2008 (JW)
 
    pure real(dp) function d1mach(i)
 
@@ -92,80 +96,72 @@ contains
 
 !*******************************************************************
 !>
-!***BEGIN PROLOGUE  I1MACH
-!***PURPOSE  Return integer machine dependent constants.
-!***LIBRARY   SLATEC
-!***CATEGORY  R1
-!***TYPE      INTEGER (I1MACH-I)
-!***KEYWORDS  MACHINE CONSTANTS
-!***Author  Fox, P. A., (Bell Labs)
-!           Hall, A. D., (Bell Labs)
-!           Schryer, N. L., (Bell Labs)
-!***DESCRIPTION
+!  Return integer machine dependent constants.
 !
 !   I1MACH can be used to obtain machine-dependent parameters for the
 !   local machine environment.  It is a function subprogram with one
 !   (input) argument and can be referenced as follows:
 !
+!```fortran
 !        K = I1MACH(I)
+!```
 !
 !   where I=1,...,16.  The (output) value of K above is determined by
 !   the (input) value of I.  The results for various values of I are
 !   discussed below.
 !
 !   I/O unit numbers:
-!     I1MACH( 1) = the standard input unit.
-!     I1MACH( 2) = the standard output unit.
-!     I1MACH( 3) = the standard punch unit.
-!     I1MACH( 4) = the standard error message unit.
+!     * `I1MACH( 1)`   = the standard input unit.
+!     * `I1MACH( 2)`   = the standard output unit.
+!     * `I1MACH( 3)`   = the standard punch unit.
+!     * `I1MACH( 4)`   = the standard error message unit.
 !
 !   Words:
-!     I1MACH( 5) = the number of bits per integer storage unit.
-!     I1MACH( 6) = the number of characters per integer storage unit.
+!     * `I1MACH( 5)` = the number of bits per integer storage unit.
+!     * `I1MACH( 6)` = the number of characters per integer storage unit.
 !
 !   Integers:
 !     assume integers are represented in the S-digit, base-A form
-!
+!```
 !                sign ( X(S-1)*A**(S-1) + ... + X(1)*A + X(0) )
 !
 !                where 0 <= X(I) < A for I=0,...,S-1.
-!     I1MACH( 7) = A, the base.
-!     I1MACH( 8) = S, the number of base-A digits.
-!     I1MACH( 9) = A**S - 1, the largest magnitude.
+!```
+!
+!     * `I1MACH( 7) = A`, the base.
+!     * `I1MACH( 8) = S`, the number of base-A digits.
+!     * `I1MACH( 9) = A**S - 1`, the largest magnitude.
 !
 !   Floating-Point Numbers:
-!     Assume floating-point numbers are represented in the T-digit,
-!     base-B form
+!     Assume floating-point numbers are represented in the T-digit, base-B form
+!```
 !                sign (B**E)*( (X(1)/B) + ... + (X(T)/B**T) )
 !
 !                where 0 <= X(I) < B for I=1,...,T,
 !                0 < X(1), and EMIN <= E <= EMAX.
-!     I1MACH(10) = B, the base.
+!```
+!     `I1MACH(10) = B`, the base.
 !
 !   Single-Precision:
-!     I1MACH(11) = T, the number of base-B digits.
-!     I1MACH(12) = EMIN, the smallest exponent E.
-!     I1MACH(13) = EMAX, the largest exponent E.
+!     * `I1MACH(11) = T`, the number of base-B digits.
+!     * `I1MACH(12) = EMIN`, the smallest exponent E.
+!     * `I1MACH(13) = EMAX`, the largest exponent E.
 !
 !   Double-Precision:
-!     I1MACH(14) = T, the number of base-B digits.
-!     I1MACH(15) = EMIN, the smallest exponent E.
-!     I1MACH(16) = EMAX, the largest exponent E.
+!     * `I1MACH(14) = T`, the number of base-B digits.
+!     * `I1MACH(15) = EMIN`, the smallest exponent E.
+!     * `I1MACH(16) = EMAX`, the largest exponent E.
 !
-!   To alter this function for a particular environment, the desired
-!   set of DATA statements should be activated by removing the C from
-!   column 1.  Also, the values of I1MACH(1) - I1MACH(4) should be
-!   checked for consistency with the local operating system.
+!### Author
+!  * Fox, P. A., (Bell Labs)
+!  * Hall, A. D., (Bell Labs)
+!  * Schryer, N. L., (Bell Labs)
 !
-!***REFERENCES  P. A. Fox, A. D. Hall and N. L. Schryer, Framework for
-!                 a portable library, ACM Transactions on Mathematical
-!                 Software 4, 2 (June 1978), pp. 177-188.
-!***ROUTINES CALLED  (NONE)
-!***REVISION HISTORY  (YYMMDD)
-!   750101  DATE WRITTEN
-!   960411  Modified for Fortran 90 (BE after suggestions by EHG).
-!   980727  Modified value of I1MACH(6) (BE after suggestion by EHG).
-!***END PROLOGUE  I1MACH
+!### Revision history
+!  * 750101  DATE WRITTEN
+!  * 960411  Modified for Fortran 90 (BE after suggestions by EHG).
+!  * 980727  Modified value of I1MACH(6) (BE after suggestion by EHG).
+!  * 220619  Modified for Fortran 2008 (JW)
 
    pure integer function i1mach(i)
 
@@ -200,57 +196,50 @@ contains
 
 !*******************************************************************
 !>
-!***BEGIN PROLOGUE  R1MACH
-!***PURPOSE  Return floating point machine dependent constants.
-!***LIBRARY   SLATEC
-!***CATEGORY  R1
-!***TYPE      SINGLE PRECISION (R1MACH-S, D1MACH-D)
-!***KEYWORDS  MACHINE CONSTANTS
-!***Author  Fox, P. A., (Bell Labs)
-!           Hall, A. D., (Bell Labs)
-!           Schryer, N. L., (Bell Labs)
-!***DESCRIPTION
-!
+!  Return floating point machine dependent constants.
+!!
 !   R1MACH can be used to obtain machine-dependent parameters for the
 !   local machine environment.  It is a function subprogram with one
 !   (input) argument, and can be referenced as follows:
 !
+!```fortran
 !        A = R1MACH(I)
+!```
 !
 !   where I=1,...,5.  The (output) value of A above is determined by
 !   the (input) value of I.  The results for various values of I are
 !   discussed below.
 !
-!   R1MACH(1) = B**(EMIN-1), the smallest positive magnitude.
-!   R1MACH(2) = B**EMAX*(1 - B**(-T)), the largest magnitude.
-!   R1MACH(3) = B**(-T), the smallest relative spacing.
-!   R1MACH(4) = B**(1-T), the largest relative spacing.
-!   R1MACH(5) = LOG10(B)
+!   * `R1MACH(1) = B**(EMIN-1)`, the smallest positive magnitude.
+!   * `R1MACH(2) = B**EMAX*(1 - B**(-T))`, the largest magnitude.
+!   * `R1MACH(3) = B**(-T)`, the smallest relative spacing.
+!   * `R1MACH(4) = B**(1-T)`, the largest relative spacing.
+!   * `R1MACH(5) = LOG10(B)`
 !
-!   Assume single precision numbers are represented in the T-digit,
-!   base-B form
-!
+!   Assume single precision numbers are represented in the T-digit, base-B form
+!```
 !              sign (B**E)*( (X(1)/B) + ... + (X(T)/B**T) )
 !
 !   where 0 <= X(I) < B for I=1,...,T, 0 < X(1), and
 !   EMIN <= E <= EMAX.
+!```
 !
 !   The values of B, T, EMIN and EMAX are provided in I1MACH as
 !   follows:
-!   I1MACH(10) = B, the base.
-!   I1MACH(11) = T, the number of base-B digits.
-!   I1MACH(12) = EMIN, the smallest exponent E.
-!   I1MACH(13) = EMAX, the largest exponent E.
+!   * `I1MACH(10) = B`, the base.
+!   * `I1MACH(11) = T`, the number of base-B digits.
+!   * `I1MACH(12) = EMIN`, the smallest exponent E.
+!   * `I1MACH(13) = EMAX`, the largest exponent E.
 !
+!### Author
+!  * Fox, P. A., (Bell Labs)
+!  * Hall, A. D., (Bell Labs)
+!  * Schryer, N. L., (Bell Labs)
 !
-!***REFERENCES  P. A. Fox, A. D. Hall and N. L. Schryer, Framework for
-!                 a portable library, ACM Transactions on Mathematical
-!                 Software 4, 2 (June 1978), pp. 177-188.
-!***ROUTINES CALLED  XERMSG
-!***REVISION HISTORY  (YYMMDD)
-!   790101  DATE WRITTEN
-!   960329  Modified for Fortran 90 (BE after suggestions by EG)
-!***END PROLOGUE  R1MACH
+!### Revision history
+!  * 790101  DATE WRITTEN
+!  * 960329  Modified for Fortran 90 (BE after suggestions by EG)
+!  * 220619  Modified for Fortran 2008 (JW)
 
    pure real(sp) function r1mach(i)
 
@@ -272,4 +261,6 @@ contains
    end function r1mach
 !*******************************************************************
 
-end module mach_routines
+!*****************************************************************************************
+   end module mach_routines
+!*****************************************************************************************
